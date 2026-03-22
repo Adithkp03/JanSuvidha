e8220ab4d3b216ceda34886761c78647e9943ddc
Adithkp03 <adithkp03@gmail.com>
2026-03-23 00:20:16 +0530

    no worries

--- suvidha/portal/src/services/firebase.js
+++ suvidha/portal/src/services/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAZZWeKRBmYJiP2o24OIvyLMQ07fBJ1rCg",
-    authDomain: "otp-sending-c2a0c.firebaseapp.com",
-    projectId: "otp-sending-c2a0c",
-    storageBucket: "otp-sending-c2a0c.firebasestorage.app",
-    messagingSenderId: "472655451250",
-    appId: "1:472655451250:web:c3d68c9fcaed3e6e9f1eeb"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
